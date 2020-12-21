4f15ca6d4c33cae43474df00ac99608381b35831
wyc <wyc@tedu.cn>
2020-12-21 21:03:49 +0800

    add files

---
+++ demo02.py
@@ -0,0 +1,12 @@
+
+
+# 获得数据
+usd = float(input("输入美元"))
+# 计算
+cny = usd * 6.697
+# 显示结果
+print(usd, "美元=", cny, "人民币")
+
+usd = float(input("输入美元"))
+cny = usd * 6.697
+print(usd, "美元=""几美元。", cny, "人民币")
---
+++ exercise02.py
@@ -0,0 +1,33 @@
+# """
+# 旅游管理系统
+# """
+# class Tour:
+#
+#
+#     def __init__(self):
+#         self.shifu="唐僧"
+#         self.datudi="孙悟空"
+#         self.ertudi="猪八戒"
+#         self.santudi="沙僧"
+#
+#     def qu(self,v):
+#         # print("取经")
+#         v.qujing()
+#
+#
+# class Qj:
+#     def qujing(self):
+#         pass
+#
+#
+# class Baigujing(Qj):
+#     def qujing(self):
+#         print("孙悟空三打白骨精")
+# class Hyanshan(Qj):
+#     def qujing(self):
+#         print("红孩儿")
+#
+#
+# a=Shitu()
+# a.qu(Baigujing())
+# a.qu(Hyanshan())
